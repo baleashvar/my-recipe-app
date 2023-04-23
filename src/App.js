8a5d5c583b14141ac4ea74e8fdf28fbd5e4c4952
baleashvar <baleashvar@github.com>
2023-04-23 22:31:40 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import RecipeList from "./components/RecipeList";
+
 import Idli from "./assets/img/idli.png";
 import Speghetti from "./assets/img/speghetti.png";
 import Tandoori from "./assets/img/tandoori.png";
